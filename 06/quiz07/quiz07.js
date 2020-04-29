dc38ac18b8b49de039728d31a2182f72c5ded7e3
asdf130700 <59083814+asdf130700@users.noreply.github.com>
2020-04-30 00:16:30 +0900

    Update quiz07.js

--- 06/quiz07/quiz07.js
+++ 06/quiz07/quiz07.js
@@ -1,10 +1,31 @@
+const  $bug  = document .getElementById ("bug");
+const  $box  = document .querySelector (".box");
+let  $point  = document .getElementById ("point");
+let  $life  = document .getElementById ("life");
 
-const $bug = document.getElementById("bug");
-const $box = document.querySelector(".box");
-let $point = document.getElementById("point");
-let $life = document.getElementById("life");
+let  point  = 0 ;
+let  life  = 10 ;
+let  clicked  = false ;
 
-let point = 0;
-let life = 10;
+    $bug .addEventListener('click', e  => {
+        point  ++;
+        $point .innerHTML  = point ;
+        });
 
-function randombug
+    $box .addEventListener('click', e  => {
+        life  --;
+        $life .innerHTML  = life ;
+        if (life ==0 ){
+            alert ("Game Over");
+}
+    })
+
+    function  randomloc () {
+        const boxleft = Math.floor(Math.random( ) * 399 ) + 1;
+        const boxtop = Math.floor(Math.random( ) * 399 ) + 1;
+        return[boxleft, boxtop];
+      }
+    setInterval(function randombug (){
+        $bug.style.boxleft = `${boxleft}px`;
+        $bug.style.boxtop = `${boxtop}px`;
+      }, 1500);
