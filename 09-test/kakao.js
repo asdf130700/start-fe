56b215c7f72433ea5c1a44a58e5b57a5d4be2717
asdf130700 <59083814+asdf130700@users.noreply.github.com>
2020-05-28 01:44:43 +0900

    09 과제

--- 09-test/kakao.js
+++ 09-test/kakao.js
@@ -1,6 +1,7 @@
 const $btn = document.querySelector('#search-btn');
 const $text = document.querySelector('#search-text');
 const $result = document.querySelector('#result');
+const $addbtn = document.querySelectorAll('#add_btn');
 const url = 'https://dapi.kakao.com/v2/search/web';
 const headers = { Authorization: 'KakaoAK 7c42a537b7a3ce28ba33f2cbf81a4246' };
 
@@ -20,19 +21,35 @@ function error() {
 }
 function search() {
   const { value } = $text;
-
   if (value === '') {
     // eslint-disable-next-line no-alert
     alert('검색어를 입력하세요');
     return false;
   }
+  getsearch();
 
-  fetch(`${url}?query=${value}`, { headers })
-    .then((res) => res.json())
-    .then(success)
-    .catch(error);
+  
 
   return true;
 }
+function getsearch(){
+  fetch(`${url}?query=${value}`, { headers })
+  .then((res) => res.json())
+  .then(success)
+  .catch(error);
+}
+function dodisplay(){
+  var con = document.getElementById("addbtn");
+  if(con.style.display=='none'){
+    con.style.display = 'block';
+  }else{
+    con.style.display = 'none';
+  }
+}
+//$addtn.style.display = 'inline-block';
 
 $btn.addEventListener('click', search);
+$addbtn.addEventListener('click',getsearch);
+  //this.parentNode.getElementsByTagName('div')[0].style.display != ''){this.parentNode.getElementsByTagName('div')[0].style.display = '';this.value = '숨기기';}else{this.parentNode.getElementsByTagName('div')[0].style.display = 'none'; this.value
+
+ )
