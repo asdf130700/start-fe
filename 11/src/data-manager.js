6d1614b1a8a8f08651975044af28f07674b08026
asdf130700 <59083814+asdf130700@users.noreply.github.com>
2020-06-14 21:57:17 +0900

    12

---
+++ 11/src/data-manager.js
@@ -0,0 +1,14 @@
+function saveData(data) {
+  console.log('todos:', data);
+  localStorage.setItem('data', JSON.stringify(data));
+}
+function loadData() {
+  const dataString = localStorage.getItem('data') || '[]';
+  return JSON.parse(dataString);
+  //   fetch('http://localhost:3000/todos').then((response) => {
+  //     response.json().then((data) => {
+  //       callback(data);
+  //     });
+  //   });
+}
+export { saveData, loadData };
--- 11/src/index.js
+++ 11/src/index.js
@@ -1,5 +1,6 @@
 import { render } from './html-render';
 import formInput from './form-input';
+import { loadData } from './data-manager';
 
 import './todos.css';
 
@@ -13,17 +14,23 @@ const $result = document.querySelector('#result');
 //   isDone: false,
 // };
 
-const todos = [
-  //   {
-  //     text: 'JS공부11',
-  //     isDone: false,
-  //   },
-  //   {
-  //     text: 'JS공부22',
-  //     isDone: false,
-  //   },
-];
+const todos = loadData();
 
+$result.addEventListener('click', (event) => {
+  const { className } = event.target;
+  if (className === 'delete') {
+    const { index } = event.target.parentElement.dataset;
+    todos.splice(index, 1);
+    render(todos);
+  } else if (className === 'toggle-checked') {
+    const { index } = event.target.parentElement.dataset;
+    todos[index].isDone = !todos[index].isDone;
+    render(todos);
+  }
+});
+
+formInput.init(todos);
+render(todos);
 // console.log($inputForm);
 // $inputForm.addEventListener('submit', (event) => {
 //   // console.log(event);
@@ -48,18 +55,3 @@ const todos = [
 // // eslint-disable-next-line no-undef
 // $result.innerHTML = `<ul>${html.join('')}</ul>`;
 // }
-
-$result.addEventListener('click', (event) => {
-  const { className } = event.target;
-  if (className === 'delete') {
-    const { index } = event.target.parentElement.dataset;
-    todos.splice(index, 1);
-    render(todos);
-  } else if (className === 'toggle-checked') {
-    const { index } = event.target.parentElement.dataset;
-    todos[index].isDone = !todos[index].isDone;
-    render(todos);
-  }
-});
-formInput.init(todos);
-render(todos);
