74dfdfe2c4a9c0142dae1c3fe1860a71b8417287
asdf130700 <59083814+asdf130700@users.noreply.github.com>
2020-06-21 22:21:37 +0900

    13주

---
+++ 13/react.js
@@ -0,0 +1,23 @@
+const rootEl = document.getElementById('root');
+const el = <div className="container">React11</div>;
+ReactDOM.render(el, rootEl);
+
+// const rootEl = document.getElementById('root');
+// const el = document.createElement('div');
+// el.textContent = 'React';
+// el.className = 'container';
+// rootEl.append(el);
+
+// const el = React.createElement('div', { className: 'container' }, 'React');
+// ReactDOM.render(el, rootEl);
+// import React from 'react';
+
+// function Header() {
+//   return (
+//     <div>
+//       <h1>Todo</h1>
+//     </div>
+//   );
+// }
+
+// export default Header;
---
+++ 13/todo/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Header from './Header';
+
+function App() {
+  // const [countNumber, setCountNumber] = useState(0);
+
+  // function addCountNumber() {
+  //   setCountNumber(countNumber + 1);
+  // }
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+// return (
+//   <div className="App">
+//     <Header />
+//     <div>{countNumber}</div>
+//     <button onClick={addCountNumber}>플러스</button>
+//   </div>
+// );
+export default App;
